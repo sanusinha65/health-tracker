598cc9a775fe4baf1d06bf20bfd3d94cb9476172
Aditya Dhanraj <sanusinha65@gmail.com>
2025-01-29 22:26:02 +0530

    Project Setup -- Installed The Required Dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}", "./node_modules/primeng/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-primeui")],
+};
