982c8383896c3790cb7f240d0645b629c1b7dbfb
Aditya Dhanraj <sanusinha65@gmail.com>
2025-01-29 22:48:57 +0530

    PrimeNG -- Setup Complete, Dashboard & Workout Pages Added, Routes Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { WorkoutComponent } from './pages/workout/workout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: DashboardComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'workout',
+        component: WorkoutComponent
+    }
+];
---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [ButtonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
